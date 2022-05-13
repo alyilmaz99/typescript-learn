898c686dec63d82ed357b75c7879bc975879f0d4
alyilmaz99 <73197677+alyilmaz99@users.noreply.github.com>
2022-05-13 22:50:51 +0300

    main docs created

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+var a = "1";
+console.log("aaa", a);
+//tsc main.ts 
+//tsc main.ts -w for watching
+// after the tsconfig.json rootDir= src
+// we dont use main.ts 
+// just use tsc -w 
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+const a = "1";
+
+console.log("aaa", a);
+
+//tsc main.ts 
+//tsc main.ts -w for watching
+
+// after the tsconfig.json rootDir= src
+// we dont use main.ts 
+// just use tsc -w 
